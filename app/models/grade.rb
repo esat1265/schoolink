34daaf125b445e6efa9baa9e50acb63edfcbafad
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-07 13:16:15 +0100

    adding grades

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,7 +2,13 @@ class Grade < ApplicationRecord
   belongs_to :course
   belongs_to :student
 
-  validates :grade, presence: true
+  validates :grade, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
+  validates :student_id, uniqueness: { scope: [:course_id, :exam_name], message: "should have only one grade per exam per course" }
   validates :date, presence: true
   validates :exam_name, presence: true
+  validate :exam_date_cannot_be_in_the_future
+
+  def exam_date_cannot_be_in_the_future
+    errors.add(:date, "can't be in the future") if date.present? && date > Date.today
+  end
 end
