550f292ca15677b8b81514f224566d62427f90c1
omar haizoun <omar.haizoun.m@gmail.com>
2024-03-11 10:24:23 +0100

    WIP

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,41 @@ class Course < ApplicationRecord
   belongs_to :teacher
 
   validates :name, presence: true
+
+  def icon
+    case self.name.downcase
+    when "french"
+      "fas fa-comments"
+    when "english"
+      "fas fa-comment-dots"
+    when "german"
+      "fas fa-comments"
+    when "maths"
+      "fas fa-square-root-alt"
+    when "physics"
+      "fas fa-atom"
+    when "biology"
+      "fas fa-dna"
+    when "chemistry"
+      "fas fa-vials"
+    when "it"
+      "fas fa-laptop-code"
+    when "sport"
+      "fas fa-football-ball"
+    when "geography"
+      "fas fa-globe-americas"
+    when "history"
+      "fas fa-landmark"
+    when "economy"
+      "fas fa-chart-line"
+    when "music"
+      "fas fa-music"
+    when "philosophy"
+      "fas fa-brain"
+    when "civic education"
+      "fas fa-balance-scale"
+    else
+      "fas fa-book-open"
+    end
+  end
 end
