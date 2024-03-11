1cf8b18756a0ab535861e6896f794b5975e6c748
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-11 17:08:55 +0100

    communications messages

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,6 @@
 class Parent < User
   has_one :student
   validates :phone_number, presence: true
-  has_many :sent_messages, foreign_key: :sender_id, class_name: 'Message'
-  has_many :received_messages, foreign_key: :receiver_id, class_name: 'Message'
+  has_many :sent_communications, foreign_key: :sender_id, class_name: 'Communication'
+  has_many :received_communications, foreign_key: :receiver_id, class_name: 'Communication'
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,6 +3,6 @@ class Teacher < User
   has_many :sections, -> { distinct }, through: :courses
   has_many :grades
   has_many :students
-  has_many :sent_messages, foreign_key: :sender_id, class_name: 'Message'
-  has_many :received_messages, foreign_key: :receiver_id, class_name: 'Message'
+  has_many :sent_communications, foreign_key: :sender_id, class_name: 'Communication'
+  has_many :received_communications, foreign_key: :receiver_id, class_name: 'Communication'
 end
