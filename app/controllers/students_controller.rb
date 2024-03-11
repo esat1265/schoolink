f552cc8ef42ea700179af4f29850baa20f50041f
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-11 10:41:39 +0100

    filtering

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @section = @student.section
     @courses = @student.grades.includes(:course).map(&:course).uniq
+    @latest_grades = @student.grades.order(created_at: :desc).limit(3)
 
     if params[:course_id].present?
       @grades = @student.grades.includes(:course).where(course_id: params[:course_id])
