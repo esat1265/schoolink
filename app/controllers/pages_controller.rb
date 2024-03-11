e539e09a9a83942d1b007cb3e77002ce9e7afb87
OliAivRo <rochat.olivia@gmail.com>
2024-03-11 09:21:30 +0100

    Teacher dashboard - Display only 5 todos list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -56,26 +56,10 @@ class PagesController < ApplicationController
       "Achievement celebration assembly",
     ]
     @todos = [
-      "Summer outing presentation",
-      "Faculty meeting, 3:00 PM, staff lounge",
-      "Submit Q2 grades by EOD",
-      "Field trip: New departure time, 9:30 AM",
-      "Parent-Teacher sign-ups",
-      "School closed tomorrow (weather)",
-      "New curriculum materials ready for pickup",
-      "Staff development day",
-      "Picture day, dress code",
       "Emergency drill this morning",
       "Tech maintenance this afternoon",
       "Congrats Science Olympiad team!",
-      "Special assembly, 9:00 AM",
-      "Review revised calendar",
-      "Supply requests",
-      "Report cards in main office",
-      "Staff luncheon today, 12:00 PM, cafeteria",
       "Parking lot construction ongoing",
-      "Health briefing, 8:30 AM",
-      "Volunteers needed for school fair",
       "Teacher of the Year nominations due",
     ]
   end
