949cd4ad4f2c94f084abaea94bf907b0e22d637c
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-12 11:53:47 +0100

    chatroom almost OK

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,4 +3,18 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def index
+    if current_user.type == "Teacher" && current_user.sections.any?
+      @chatrooms = Chatroom.where(teacher: current_user)
+      @parents = current_user.sections.map(&:students).flatten.map(&:parent).uniq
+    elsif current_user.type == "Parent"
+      @chatrooms = Chatroom.where(parent: current_user)
+      @teachers = current_user.student.courses.map(&:teacher).uniq
+    end
+  end
+
+  def create
+
+  end
 end
