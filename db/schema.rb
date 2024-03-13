2393c8382daf924933e6dcc3c83d492c41da22d4
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-13 09:07:48 +0100

    messages count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_094141) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_075157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_094141) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -77,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_094141) do
     t.string "last_name"
     t.string "type"
     t.bigint "section_id"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["parent_id"], name: "index_users_on_parent_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
