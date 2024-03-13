9767bfb796f0e7a44bfdaedecb9ca377ea4ea5be
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-13 12:56:34 +0100

    communication

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -21,14 +21,6 @@ class ChatroomsController < ApplicationController
           unread_messages_count: unread_messages_count
         }
       end
-      # Trouver tous les étudiants associés à cet enseignant à travers les cours.
-      students = Student.joins(:courses).where(courses: {teacher_id: current_user.id}).distinct
-      # En utilisant les étudiants, trouver les parents uniques et trier par section puis par last_name des parents
-      @parents = students.map(&:parent).compact.uniq.sort_by { |parent| [parent.student.section_id, parent.last_name] }
-      if params[:search].present?
-        search_term = "%#{params[:search]}%"
-        @parents = students.map(&:parent).uniq.compact
-      end
     elsif current_user.type == "Parent"
       current_user.chatrooms.includes(:teacher, :messages).each do |chatroom|
         unread_messages_count = chatroom.messages.where.not(user: current_user).where(read: false).count
