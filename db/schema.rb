dbe9871eb5fbe5da8bae3901a55f737d9a7be837
OliAivRo <rochat.olivia@gmail.com>
2024-03-14 09:04:52 +0100

    TEACHER --> Student grades > Add colors to grades

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_123900) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_124007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,7 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_123900) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "phone_number"
+    t.string "phone_number"
     t.bigint "parent_id"
     t.string "first_name"
     t.string "last_name"
