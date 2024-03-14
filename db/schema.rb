0ebc0a4d0b787e18104eda38fc7673cc25d950af
RonanKer29 <kervella.ronan@hotmail.com>
2024-03-14 10:14:33 +0100

    change phone number to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_075157) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_14_091335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,12 +101,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_075157) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "phone_number"
+    t.string "phone_number"
     t.bigint "parent_id"
     t.string "first_name"
     t.string "last_name"
     t.string "type"
     t.bigint "section_id"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["parent_id"], name: "index_users_on_parent_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
